e3e81647e1867b34107f0f435846e71874b12c07
Michael Guerzhoy <guerzh@gmail.com>
2015-10-20 20:48:41 -0400

    modelling individual stores (commented out

--- rossman_ab.py
+++ rossman_ab.py
@@ -8,10 +8,17 @@ from numpy import *
 from sklearn.linear_model import *
 from sklearn.preprocessing import OneHotEncoder
 
-DATA_DIR = 'C:/Users/Guerzhoy/Desktop/utds/rossmann'
+import time
+
+DATA_DIR = 'C:/Users/Guerzhoy/Desktop/utds/rossmann/'
 os.chdir(DATA_DIR)
 
 random.seed(0)
+
+def convert_date(date_str):
+    pattern = '%Y-%m-%d'
+    epoch = int(time.mktime(time.strptime(date_str, pattern)))
+    return epoch//86400
 
 
 def get_data_categ(data):
@@ -26,18 +33,25 @@ def get_data_categ(data):
     data_categ = concat([data_categ, get_dummies(data.loc[:,"SchoolHoliday"], prefix = ["SchH"])], axis = 1)
     data_categ = concat([data_categ, get_dummies(data.loc[:,"Promo"], prefix = ["PROMO"])], axis = 1)
     data_categ = concat([data_categ, get_dummies(data.loc[:,"Open"], prefix = ["OPEN"])], axis = 1)
+    #data_categ = concat([data_categ, get_dummies(data.loc[:,"month"], prefix = ["MONTH"])], axis = 1)
     data_categ = data_categ.fillna(0)
     return data_categ
     
 
 
 data = read_csv("data/train.csv", low_memory=False)
+data = data.loc[data["Sales"]!=0,:]
+
 store_means = DataFrame(data.groupby("Store").median()["Sales"])
 store_means["Store"] = store_means.index
 store_means.columns = ["SalesMean", "Store"]
 data = data.merge(store_means, on = ["Store"])
 
 
+#data["epoch"] = data["Date"].apply(convert_date)
+#data["month"] = data["epoch"]//12
+
+#read in the store data
 store_data = read_csv("data/store.csv")
 
 
@@ -51,6 +65,9 @@ data_cols = list(data_categ.columns)
 
 data_test = read_csv("data/test.csv")
 data_test = data_test.merge(store_means, on = ["Store"])
+#data_test["epoch"] = data_test["Date"].apply(convert_date)
+#data_test["month"] = data_test["epoch"]//12
+
 
 data_test_categ = get_data_categ(data_test)
 data_test_categ = concat([data_test_categ, data_test["SalesMean"]], axis = 1)
@@ -62,9 +79,20 @@ data_test_x = data_test_categ.loc[:, data_cols]
 data_x = data_categ.loc[:, data_cols]
 data_y = data.loc[:, ["Sales"]]
 
+random.seed(0)
 idx = random.permutation(range(len(data_x)))
 idx_train = idx[:int(.9*len(idx))]
+
+##############################################################################
+#idx_train = idx_train[where(data.loc[idx_train, "Store"] == 50)]
+##############################################################################
+
 idx_valid =  idx[int(.9*len(idx)):]
+
+###############################################################################
+#idx_valid = idx_valid[where(data.loc[idx_valid, "Store"] == 50)]
+###############################################################################
+
 
 train_x = data_x.loc[idx_train,:]
 train_x.index = range(len(train_x))
@@ -81,7 +109,7 @@ valid_x["SalesMean"] = log(1+valid_x["SalesMean"])
 data_test_x["SalesMean"] = log(1+data_test_x["SalesMean"])
 
 lr = RidgeCV(alphas= [.75, 1, 1.25, 1.5, 2, 10, 20, 100])
-lr.fit(train_x.as_matrix(), log(1+train_y.as_matrix().reshape((len(train_y),))))
+lr.fit(train_x.as_matrix(), log(1+train_y.as_matrix().reshape((len(train_y),))) )
 
 df = DataFrame()
 df["var"] = valid_x.columns
@@ -93,13 +121,18 @@ actual_valid = valid_y.as_matrix().reshape((len(valid_y),))
 
 pred_valid = pred_valid[actual_valid>0]
 actual_valid = actual_valid[actual_valid>0]
-#validation RMSPE
+
+
+#validation RMSPE .23966
+
+#store 50: .22
 sqrt(mean( ((pred_valid-actual_valid)/actual_valid)**2 ))
+
 
 #sqrt(mean((lr.predict(train_x.as_matrix()).flatten()-train_y.as_matrix().flatten())**2))
 #sqrt(mean((lr.predict(valid_x.as_matrix()).flatten()-valid_y.as_matrix().flatten())**2))
 
-submit = True
+submit = False
 if submit:
     submission = DataFrame()
     submission["Id"] = data_test["Id"]
